1d6f6018df931c6998b11fb4c2fbd6b58d798b83
Dvotch <mr.legenda16@mail.ru>
2024-02-12 02:29:59 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import { Button } from "@/shared/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const handleClick = () => {
+    setTestotserone((e) => e + Math.random());
+  };
+
+  const [testosterone, setTestotserone] = useState(0);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Button</Button>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <Button onClick={handleClick}>
+        У меня умирал сервер и я ничего не мог сделать, нажми на меня, чтобы
+        вырос тестостерон
+      </Button>
+      {!!testosterone && (
+        <p className="text-red-700 mt-10">Твой тестостерон {testosterone}</p>
+      )}
     </main>
   );
 }
