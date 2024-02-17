03474b633821d648ce83d1efa8bf70f7fb54c45f
dvotch <dvotch@mail.ru>
2024-02-17 14:52:30 +0300

    feat: add h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-
-
 import { CoursesList } from "@/features/courses-list/pub/courses-list";
 import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form";
-
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
-      <CreateCourseForm revalidatePagePath="/" className="max-w-[300px] mb-10" />
+      <h1>Courses</h1>
+      <CreateCourseForm
+        revalidatePagePath="/"
+        className="max-w-[300px] mb-10"
+      />
       <CoursesList revalidatePagePath="/" />
     </main>
   );
