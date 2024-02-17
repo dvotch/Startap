8ae1d287b78bbbf0c09aeb62a4c8cb891bd459f8
dvotch <dvotch@mail.ru>
2024-02-18 00:54:22 +0300

    adding theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-
-
 import { CoursesList } from "@/features/courses-list/pub/courses-list";
 import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form";
-
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
-      <CreateCourseForm revalidatePagePath="/" className="max-w-[300px] mb-10" />
+      <CreateCourseForm
+        revalidatePagePath="/"
+        className="max-w-[300px] mb-10"
+      />
       <CoursesList revalidatePagePath="/" />
     </main>
   );
---
+++ src/widgets/app-header/_ui/profile.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Avatar, AvatarFallback } from "@/shared/ui/avatar";
+import { Button } from "@/shared/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/shared/ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { LogOut, User } from "lucide-react";
+import Link from "next/link";
+
+export function Profile() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="p-px rounded-full self-center h-8 w-8"
+        >
+          <Avatar className="h-8 p-8">
+            <AvatarFallback>AC</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 mr-2">
+        <DropdownMenuLabel>
+          <p>Мой аккаунт</p>
+          <p className="text-xs text-muted-foreground overflow-hidden text-ellipsis">
+            Dvotch
+          </p>
+        </DropdownMenuLabel>
+        <DropdownMenuGroup></DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href={`/profile/1`}>
+              <User className="mr-2 h-4 w-4" />
+              <span>Профиль</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Выход</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
